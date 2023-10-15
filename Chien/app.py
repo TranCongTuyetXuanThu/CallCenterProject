c6acac23f80bc9fc5f2f617658c82f01c4bd2be6
chiennguyen383 <125630702+chiennguyen383@users.noreply.github.com>
2023-10-15 13:46:01 +0700

    Add files via upload

---
+++ Chien/app.py
@@ -0,0 +1,35 @@
+from flask import Flask ,redirect, url_for, render_template, request
+import os
+
+app = Flask(__name__)
+
+# @app.route('/index') 
+# def hello_world():
+#     return "<h1>Chien</h1>"
+
+# @app.route('/Blog/<int:blog_id>') 
+# def hello_blog(blog_id):
+#     return f"<h1> Blog {blog_id}!</h1>"
+
+# @app.route('/user/<name>') 
+# def hello_user(name):
+#     if name == 'admin':
+#         return redirect(url_for('hello_world'))
+#     return f"<h1> Hello {name}!</h1>"
+
+@app.route('/') 
+def index():
+    return render_template("index.html")
+@app.route('/upload', methods=['POST'])
+def upload():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        file_path = os.path.join('uploads', uploaded_file.filename)
+        #uploaded_file.save(file_path)
+        # Xử lý file âm thanh và triết xuất kết quả từ mô hình máy học ở đây
+        # Sau đó trả kết quả cho người dùng
+        return "Kết quả: ..."
+    return "Không có file được tải lên."
+
+if __name__ == "__main__":
+    app.run(debug = True)
