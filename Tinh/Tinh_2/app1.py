675dc56be843f9e69359e1bf05e09e8aa74fd24f
hdtinh57 <125421251+hdtinh57@users.noreply.github.com>
2023-10-24 13:31:28 +0700

    new folder of web

---
+++ Tinh/Tinh_2/app1.py
@@ -0,0 +1,58 @@
+import os
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+app = Flask(__name__)
+
+# Thư mục lưu trữ tệp tải lên và thư mục lưu trữ biểu đồ
+UPLOAD_FOLDER = 'uploads'
+STATIC_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['STATIC_FOLDER'] = STATIC_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+if not os.path.exists(STATIC_FOLDER):
+    os.makedirs(STATIC_FOLDER)
+
+@app.route('/home')
+def home_screen():
+    return render_template('home.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST' and 'audio_file' in request.files:
+        file = request.files['audio_file']
+        if file.filename == '':
+            return redirect(request.url)
+
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+
+        y, sr = librosa.load(file_path)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr)
+
+        plt.figure(figsize=(10, 4))
+        librosa.display.specshow(mfccs, x_axis='time')
+        plt.colorbar()
+        plt.title('MFCC')
+        plt.xlabel('Thời gian')
+        plt.ylabel('MFCC Coefficients')
+
+        image_path = os.path.join(app.config['STATIC_FOLDER'], 'mfcc.png')
+        plt.savefig(image_path)
+
+        return render_template('index1.html', mfcc_image='mfcc.png')
+
+    return render_template('index1.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ Tinh/Tinh_2/run.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home_screen():
+    return render_template('home.html')
+@app.route('/SER')
+def predict_screen():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Check if a file is included in the form submission
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    # Check if the file is selected
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    if file:
+        # Preprocess the file (if needed) and pass it to your model for prediction
+        # result = model.predict(file)
+
+        # Display the result on the webpage
+        # return render_template('result.html', result=result)
+        return render_template('result.html')
+    
+
+if __name__ == "__main__":
+    app.run(debug = True)
